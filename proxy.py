961ba87f7c51903cb42d3f76703fe5e9d5de07a7
Umer Ahmed <57960455+Teakay23@users.noreply.github.com>
2022-04-30 15:10:47 +0500

    added scheduling algorithms for cache

--- proxy.py
+++ proxy.py
@@ -3,8 +3,14 @@ import threading
 import time
 import sys
 import os
+import glob
 
 blacklist = []
+priorityQueue = []
+roundRobinQueue = []
+roundRobinDictionary = dict()
+schedulingMode = 'round_robin'
+visitThreshold = 3
 
 def start(portNum):
     try:
@@ -73,6 +79,8 @@ def readRequest(connection, address):
 
 
 def get_request(requestServer, requestPort, connection, receivedRequest, address, file):
+    global priorityQueue, roundRobinDictionary, roundRobinQueue
+
     file = file.replace(b".", b"_").replace(b"http://", b"_").replace(b"/", b"")
 
     try:
@@ -83,6 +91,37 @@ def get_request(requestServer, requestPort, connection, receivedRequest, address
         file_handler = open(my_file, 'rb')
 
         print("Cache Hit")
+
+        if schedulingMode == 'fifo':
+            insertion = str(file)
+            priorityQueue.remove(insertion)
+            priorityQueue.append(insertion)
+
+            fifo_file_path = os.path.join(THIS_FOLDER, 'fifo.txt')
+            fifo_file = open(fifo_file_path, 'w')
+
+            for site in priorityQueue:
+                fifo_file.write(site + '\n')
+
+            fifo_file.close()
+
+        if schedulingMode == 'round_robin':
+            if str(file) in roundRobinDictionary.keys():
+                roundRobinDictionary[str(file)] += 1
+            else:
+                roundRobinDictionary[str(file)] = 1
+
+            roundRobinQueue.remove(str(file))
+            roundRobinQueue.append(str(file))
+
+            roundRobin_file_path = os.path.join(THIS_FOLDER, 'round_robin.txt')
+            roudnRobin_file = open(roundRobin_file_path, 'w')
+
+            for site in roundRobinQueue:
+                roudnRobin_file.write(site + '\n')
+
+            roudnRobin_file.close()
+
         cached_material = file_handler.readlines()
         file_handler.close()
         # connection.send("HTTP/1.0 200 OK\r\n".encode('utf-8'))            
@@ -108,15 +147,71 @@ def get_request(requestServer, requestPort, connection, receivedRequest, address
             file_object = s.makefile('rb', 0)
             buff = file_object.readlines()
 
-            print("creating cache file")
-            my_file = os.path.join(THIS_FOLDER, 'cache\\' + str(file))
-            temp_file = open(my_file, "wb+")
+            cacheFlag = False
+
+            if schedulingMode == 'fifo':
+                if len(priorityQueue) >= 10:
+                    removal = priorityQueue.pop(0)
+
+                    delete_file = os.path.join(THIS_FOLDER, 'cache\\' + removal)
+
+                    if os.path.exists(delete_file):
+                        os.remove(delete_file)
+
+                insertion = str(file)
+                priorityQueue.append(insertion)
+
+                fifo_file_path = os.path.join(THIS_FOLDER, 'fifo.txt')
+                fifo_file = open(fifo_file_path, 'w')
+
+                for site in priorityQueue:
+                    fifo_file.write(site + '\n')
+
+                fifo_file.close()
+
+                cacheFlag = True
+
+            if schedulingMode == 'round_robin':
+                strFile = str(file)
+                if strFile in roundRobinDictionary.keys():
+                    roundRobinDictionary[strFile] += 1
+                else:
+                    roundRobinDictionary[strFile] = 1
+
+                if roundRobinDictionary[strFile] > visitThreshold:
+                    if len(roundRobinQueue) >= 10:
+                        removal = roundRobinQueue.pop(0)
+
+                        delete_file = os.path.join(THIS_FOLDER, 'cache\\' + removal)
+
+                        if os.path.exists(delete_file):
+                            os.remove(delete_file)
+
+                    insertion = str(file)
+                    roundRobinQueue.append(insertion)
+
+                    roundRobin_file_path = os.path.join(THIS_FOLDER, 'round_robin.txt')
+                    roundRobin_file = open(roundRobin_file_path, 'w')
+
+                    for site in roundRobinQueue:
+                        roundRobin_file.write(site + '\n')
+
+                    roundRobin_file.close()
+
+                    cacheFlag = True
+
+            if cacheFlag == True:
+                print("creating cache file")
+                my_file = os.path.join(THIS_FOLDER, 'cache\\' + str(file))
+                temp_file = open(my_file, "wb+")
 
             for line in buff:
-                temp_file.write(line)
+                if cacheFlag == True:
+                    temp_file.write(line)
                 connection.send(line)
-
-            print("cache file created")
+            
+            if cacheFlag == True:
+                print("cache file created")
 
             print("Request of client " + str(address) + " completed. ")
             s.close()
@@ -128,6 +223,8 @@ def get_request(requestServer, requestPort, connection, receivedRequest, address
 
 
 def connect_request(requestServer, requestPort, connection, receivedRequest, address, file):
+    global priorityQueue, roundRobinDictionary, roundRobinQueue
+
     file = file.replace(b".", b"_").replace(b"http://", b"_").replace(b"/", b"")
 
     try:
@@ -158,6 +255,14 @@ def connect_request(requestServer, requestPort, connection, receivedRequest, add
             reply = "HTTP/1.0 200 Connection established\r\n"
             reply += "Proxy-agent: K190181_K191449\r\n"
             reply += "\r\n"
+
+            # print("creating cache file")
+
+            # THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+            # my_file = os.path.join(THIS_FOLDER, 'cache\\' + str(file))
+            # temp_file = open(my_file, "wb+")
+            # temp_file.write(reply.encode())
+            
             connection.sendall(reply.encode())
 
         except socket.error as err:
@@ -172,14 +277,21 @@ def connect_request(requestServer, requestPort, connection, receivedRequest, add
             try:
                 receivedRequest = connection.recv(4096)
                 s.sendall(receivedRequest)
+                # print("HTTPS Non blocking Retrieval successfull")c
             except socket.error as err:
                 pass
+                # print("ending non blocking socket")
+                # break
 
             try:
                 reply = s.recv(4096)
+                # temp_file.write(reply)
                 connection.sendall(reply)
-            except socket.error as e:
+                # print("HTTPS Non blocking Receival successfull")
+            except socket.error as err:
                 pass
+                # print("ending non blocking connection")
+                # break
 
 
 if __name__ == "__main__":
@@ -200,13 +312,58 @@ if __name__ == "__main__":
 
     blacklist_file.close()
 
+    my_file = os.path.join(THIS_FOLDER, 'fifo.txt')
+
+    try:
+        fifo_file = open(my_file, 'r')
+    except FileNotFoundError:
+        fifo_file = open(my_file, 'w+')
+
+    lines = fifo_file.readlines()
+
+    for line in lines:
+        if line != '\n':
+            line = line.replace('\n', '')
+            priorityQueue.append(line)
+
+    fifo_file.close()
+
+    my_file = os.path.join(THIS_FOLDER, 'round_robin.txt')
+
+    try:
+        roundRobin_file = open(my_file, 'r')
+    except FileNotFoundError:
+        roundRobin_file = open(my_file, 'w+')
+
+    lines = roundRobin_file.readlines()
+
+    for line in lines:
+        if line != '\n':
+            line = line.replace('\n', '')
+            roundRobinQueue.append(line)
+
+    roundRobin_file.close()
+
+    my_file = os.path.join(THIS_FOLDER, 'scheduleingMode.txt')
+
+    try:
+        schedFile = open(my_file, 'r')
+        schedulingMode = schedFile.read()
+        print(schedulingMode)
+    except FileNotFoundError:
+        schedFile = open(my_file, 'w+')
+        schedFile.write(schedulingMode)
+    schedFile.close()
+    
+
     while 1:
         os.system("cls")
         print("Enter a number to select:\n")
         print("1. Filter content (Add Site/IP and websites to blacklist)")
         print("2. Allow content (Remove Site/IP and websites from blacklist)")
-        print("3. Start proxy server.")
-        print("4. Exit")
+        print("3. Choose cache scheduling method.")
+        print("4. Start proxy server.")
+        print("5. Exit")
         inp = int(input("\nSelection: "))
 
         if inp == 1:
@@ -258,9 +415,83 @@ if __name__ == "__main__":
                 break
 
         elif inp == 3:
-            start(6543)
+            while 1:
+                os.system('cls')
+                print('Choose a scheduling algorithm. WARNING: All previous cache will be deleted.')
+                print("\n1. FIFO")
+                print("2. Round Robin")
+
+                inp2 = input('Enter a number to select, press b to go back: ')
+                if inp2 == 'b':
+                    break
+                if inp2 == '1':
+                    schedulingMode = 'fifo'
+                    priorityQueue = []
+                    roundRobinQueue = []
+                    roundRobinDictionary = dict()
+
+                    my_file = os.path.join(THIS_FOLDER, 'scheduleingMode.txt')
+                    schedFile = open(my_file, 'w')
+                    schedFile.write(schedulingMode)
+                    schedFile.close()
+
+                    my_file = os.path.join(THIS_FOLDER, 'cache\\*')
+                    files = glob.glob(my_file)
+                    for f in files:
+                        os.remove(f)
+
+                    my_file = os.path.join(THIS_FOLDER, 'fifo.txt')
+                    temp = open(my_file, 'w')
+                    temp.close()
+
+                    my_file = os.path.join(THIS_FOLDER, 'round_robin.txt')
+                    temp = open(my_file, 'w')
+                    temp.close()
+
+                    print("Cache scheduling changed to FIFO. All previous cache wiped out.")
+                    print("Press Enter to continue.")
+                    input()
+                    break
+
+                elif inp2 == '2':
+                    schedulingMode = 'round_robin'
+                    priorityQueue = []
+                    roundRobinQueue = []
+                    roundRobinDictionary = dict()
+
+                    my_file = os.path.join(THIS_FOLDER, 'scheduleingMode.txt')
+                    schedFile = open(my_file, 'w')
+                    schedFile.write(schedulingMode)
+                    schedFile.close()
+
+                    my_file = os.path.join(THIS_FOLDER, 'cache\\*')
+                    files = glob.glob(my_file)
+                    for f in files:
+                        os.remove(f)
+
+                    my_file = os.path.join(THIS_FOLDER, 'fifo.txt')
+                    temp = open(my_file, 'w')
+                    temp.close()
+
+                    my_file = os.path.join(THIS_FOLDER, 'round_robin.txt')
+                    temp = open(my_file, 'w')
+                    temp.close()
+
+                    print("Cache scheduling changed to Round Robin.3 All previous cache wiped out.")
+                    print("Press Enter to continue.")
+                    input()
+                    break
+                
+                else:
+                    print("Invalid input. Please try again.")
+                    print("Press Enter to continue.")
+                    input()
+                    continue
 
         elif inp == 4:
+            start(6543)
+
+        elif inp == 5:
             break
 
         else:
